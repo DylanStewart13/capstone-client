3bf1bc9652018396c4d316efa12e5a1a583baa49
dylanstewart13 <stewart.dylan.m@gmail.com>
2023-01-02 22:50:35 -0700

    nvm

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 let url = 'https://capstone-proj.herokuapp.com';
 
 axios.defaults.baseURL = url;
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('JWT_PAYLOAD');
 
 ReactDOM.render(
